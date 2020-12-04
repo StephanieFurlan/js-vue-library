d18dc07ee3752fed830e7d18e54581b7fb24c77d
Stephanie <stephanie.furlan94@gmail.com>
2020-12-04 22:22:09 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,63 @@
+
+Vue.component('book-list', {
+   props: [
+      'books'
+   ],
+   template: `
+      <ul class="book-list">
+         <li>
+            <div>TITLE</div>
+            <div>AUTHOR</div>
+            <div>YEAR</div>
+         </li>
+         <li v-for="book in books">
+            <div>{{book.title}}</div>
+            <div>{{book.author}}</div>
+            <div>{{book.year}}</div>
+         </li>
+      </ul>
+      `
+})
+
+var app = new Vue({
+   el: '#app',
+   component: [
+      'book-list'
+   ],
+   data: {
+      title: '',
+      author: '',
+      year: '',
+      books : [
+      	{
+      		title: "Il vecchio e il mare",
+      		author: "Ernest Hemingway",
+      		year: 1951
+      	},
+      	{
+      		title: "La forma dell'acqua",
+      		author: "Andrea Camilleri",
+      		year: 1994
+      	},
+      	{
+      		title: "Elogio della follia",
+      		author: "Henri Laborit",
+      		year: 1976
+      	},
+      	{
+      		title: "La camera azzurra",
+      		author: "George Simenon",
+      		year: 1964
+      	}
+      ]
+   },
+   methods: {
+      addBook: function() {
+         this.books.push({
+            title: this.title,
+            author: this.author,
+            year: this.year
+         });
+      }
+   }
+})
